5162ec50354d1b8fc80ed8dabc1e69f53ca19cc9
mymac <mymac@Eruquls-Mac.local>
2020-12-08 09:49:19 +0200

    initialize2

---
+++ PDFPencilKit/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PDFPencilKit
+//
+//  Created by mymac on 2020/12/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
